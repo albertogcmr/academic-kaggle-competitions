6466b9dab431a9e386d3ac316267efdc74a8bd56
noeliacarrion <ncs.933@gmail.com>
2019-11-07 23:14:33 +0100

    add gitignore, some changes to check

--- script/source/functions.py
+++ script/source/functions.py
@@ -4,38 +4,39 @@ import numpy as np
 
 
 def loadData(path):
-    data = pd.read_csv(path, index_col=0)
+    data = pd.read_csv (path, index_col=0)
     return data
 
 
 def cleaningData(data, size):
     if size is None:
-        data = data.sample(n=len(data))
+        data = data.sample (n=len (data))
     else:
-        data = data.sample(n=size)
-    data.reset_index(inplace=True)
-    data.rename(columns={"index": "Id"}, inplace=True)
+        data = data.sample (n=size)
+    data.reset_index (inplace=True)
+    data.rename (columns={"index": "Id"}, inplace=True)
     return data
 
 
 def splitData(data, target):
     y = data[[target]]
-    x = data[[i for i in list(data.columns) if i != target]]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.35, random_state=42)
+    x = data[[i for i in list (data.columns) if i != target]]
+    x_train, x_test, y_train, y_test = train_test_split (x, y, test_size=0.35, random_state=42)
     x_train.loc[:, target] = y_train
     x_test.loc[:, target] = y_test
     test_students_columns = [col for col in x_test.columns if col != target]
     test_students = x_test[test_students_columns]
     test_solution_columns = [col for col in x_test.columns if (col == target or col == 'Id' or col == 'id')]
     test_solution = x_test[test_solution_columns]
-    sample_submission = test_solution.copy()
-    sample_submission[target] = np.random.randint(y_train[target].min(), y_train[target].max(), size=(len(test_solution),1))
+    sample_submission = test_solution.copy ()
+    sample_submission[target] = np.random.randint (y_train[target].min (), y_train[target].max (),
+                                                   size=(len (test_solution), 1))
     return x_train, test_students, test_solution, sample_submission
 
 
 def saveData(path, x_train, test_students, test_solution, sample_submission):
-    x_train.to_csv(path + '/train_students.csv', index=False)
-    test_students.to_csv(path + '/test_students.csv', index=False)
-    test_solution.to_csv(path + '/test_solution.csv', index=False)
-    sample_submission.to_csv(path + '/sample_submission.csv', index=False)
+    x_train.to_csv (path + '/data_students.csv', index=False)
+    test_students.to_csv (path + '/test_students.csv', index=False)
+    test_solution.to_csv (path + '/test_solution.csv', index=False)
+    sample_submission.to_csv (path + '/sample_submission.csv', index=False)
     return x_train, test_students, test_solution, sample_submission
--- script/source/main.py
+++ script/source/main.py
@@ -4,36 +4,39 @@ import os
 import warnings
 
 
-def get_args() :  # add arguments to prepare the data
+def get_args():  # add arguments to prepare the data
     parser = argparse.ArgumentParser (description="Name of the csv file")
-    parser.add_argument ('-n', '--name', help="type the name of the csv file", type=str)
-    parser.add_argument ('-t', '--target', help="type the name of the target column", type=str)
-    parser.add_argument ('-s', '--size', help="If your data is too big, choose the number of records", type=int,
+    parser.add_argument ("-n", "--name", help="type the name of the csv file", type=str)
+    parser.add_argument ("-t", "--target", help="type the name of the target column", type=str)
+    parser.add_argument ("-s", "--size", help="If your data is too big, choose the number of records", type=int,
                          required=False)
     return parser.parse_args ()
 
 
-def menu() :
-    print ('''--------Introduce the name of the csv file, the target column------
+def menu():
+
+    print ('''-------- Introduce the name of the csv file and the target column --------------
 
     Format required:
 
-    ------------- main.py -n "cars.csv" -t "price" -s 170000 ------------''')
+    ------------- main.py -n "diamonds.csv" -t "price" -s 170000 ------------
+           
+    -------------- The argument size is optional ------------------''')
 
 
-def main() :
+def main():
     menu ()
     warnings.filterwarnings ("ignore")
     args = get_args ()
     name, target, size = args.name, args.target, args.size
     print ("loading the dataset...")
-    data = loadData ('./input/{}'.format (name))
+    data = loadData ('../input/{}'.format (name))
     print ("cleaning the dataset...")
     data_clean = cleaningData (data, size)
     print ("splitting into train and test the dataset...")
     x_train, test_students, test_solution, sample_submission = splitData (data_clean, target)
-    saveData ("./output", x_train, test_students, test_solution, sample_submission)
+    saveData ("../output", x_train, test_students, test_solution, sample_submission)
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main ()
